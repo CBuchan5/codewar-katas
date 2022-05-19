a21ec728139ba2f1abfa7623a063949633abb5c8
charlotte buchan <cebuc@talktalk.net>
2022-05-19 19:24:20 +0100

    updated js test

--- fix_string/fixString.js
+++ fix_string/fixString.js
@@ -1,4 +1,8 @@
 const fixString = (characters) =>  {
+    characters.split();
+    console.log("LOOOOOOOK HERE!!")
+
+    console.log(characters)
 return characters
 }
 
--- fix_string/fixString.test.js
+++ fix_string/fixString.test.js
@@ -1,8 +1,12 @@
 const fixString = require('./fixString')
 describe ("String", () => {
 
-it("has a string", () => {
+it('has a string', () => {
     expect(fixString('string')).toBe('string')
+});
 
+it('it converts a string all to lower case characters', () => {
+    expect(fixString('coDe')).toBe('code')
 });
 })
+
